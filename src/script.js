1cb6c8eaf5a518927e16feef0de3d44d22f2885e
KamysDev <sereginka070504@gmail.com>
2023-10-14 13:13:10 +0700

    major fixes

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-
 let bg = document.querySelector('.mouse-parallax-bg');
 window.addEventListener('mousemove', function(e) 
 {
@@ -6,6 +5,8 @@ window.addEventListener('mousemove', function(e)
     let y = e.clientY / window.innerHeight;  
     bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 });
+
+///////////////////////////////////////////////////////////////////////////////////
 
 function myFunction()
 {
@@ -16,12 +17,11 @@ window.onclick = function (event)
 {
     if (!event.target.matches('.dropbtn'))
     {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++)
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++)
         {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show'))
-            {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
         }

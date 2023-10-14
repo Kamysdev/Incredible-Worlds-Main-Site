5a749974ba98d0cbf321891751cbb656396ca3e4
KamysDev <sereginka070504@gmail.com>
2023-10-14 16:51:03 +0700

    123

--- src/script.js
+++ src/script.js
@@ -28,3 +28,20 @@ window.onclick = function (event)
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////////
+
+
+let anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        let blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    })
+}

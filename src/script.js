e87ba39e3408549ca709af882fe910887351a452
Minory54 <72006176+Minory54@users.noreply.github.com>
2023-10-14 16:54:35 +0700

    Added smooth scroll

--- src/script.js
+++ src/script.js
@@ -28,3 +28,15 @@ window.onclick = function (event)
     }
 }
 
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
